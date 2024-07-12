e7a0f1442e0960fa8cda172ac2c6cfe6976edfd7
n-marcelino <nicomarcelino0613@gmail.com>
2024-07-12 20:52:09 +0800

    adding edit func

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,13 @@ import {initializeApp} from 'firebase/app'
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRwWd1E7JJiSuHK-J3FRXj8wjK3-WO84E",
-    authDomain: "reactdemojuly2024.firebaseapp.com",
-    projectId: "reactdemojuly2024",
-    storageBucket: "reactdemojuly2024.appspot.com",
-    messagingSenderId: "140474381397",
-    appId: "1:140474381397:web:314537f93c0cd60f1090dc"
+  apiKey: "AIzaSyDmGOSaorZkHMbzp8ld3xZCT7qhJyuLpYo",
+  authDomain: "reactdemo2024.firebaseapp.com",
+  projectId: "reactdemo2024",
+  storageBucket: "reactdemo2024.appspot.com",
+  messagingSenderId: "507028973259",
+  appId: "1:507028973259:web:d96161a8975149d181dd4e",
+  measurementId: "G-SLH99G44SL"
 };
 
   initializeApp(firebaseConfig);
@@ -15,3 +16,5 @@ const firebaseConfig = {
   const db = getFirestore();
 
   export {db}
+
+  
